f4f3083c11a9bd2f5881b4ac00c65ac2c1e0c85c
Okinawa <fitthep@gmail.com>
2023-09-30 17:16:53 +0700

    finished

--- src/app/layout.js
+++ src/app/layout.js
@@ -91,8 +91,8 @@ export default function RootLayout({ children }) {
               </Title>
               {children}
               <Footer
-                studentId="650610560"
-                fullName="Chayanin Suatap"
+                studentId="6550612079"
+                fullName="Kittiwat Yasarawan"
                 year="2023"
               />
             </Container>
